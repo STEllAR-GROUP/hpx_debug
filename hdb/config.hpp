c7fc7728fbdd0e04511a6a2d5b0ee0ac3f4e6cdc
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2013-09-16 14:12:34 -0500

    Adding missing file

---
+++ hdb/config.hpp
@@ -0,0 +1,11 @@
+//  Copyright (c) 2013 Hartmut Kaiser
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(HPX_DEBUG_CONFIG_SEP_16_2013_1054AM)
+#define HPX_DEBUG_CONFIG_SEP_16_2013_1054AM
+
+#include <hpx/config/defines.hpp>
+
+#endif
