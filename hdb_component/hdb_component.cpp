916da3137db1b3050fcb43161e4440dc932ad8bf
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2014-03-15 10:24:54 -0500

    Adapting component loading to new dynamic loading macros

--- hdb_component/hdb_component.cpp
+++ hdb_component/hdb_component.cpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2013 Hartmut Kaiser
+//  Copyright (c) 2014 Hartmut Kaiser
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -11,14 +11,14 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 // Add factory registration functionality.
-HPX_REGISTER_COMPONENT_MODULE();
+HPX_REGISTER_COMPONENT_MODULE_DYNAMIC();
 
 ///////////////////////////////////////////////////////////////////////////////
 typedef hpx::components::simple_component<
     hpx_debug::server::hdb_component
 > hdb_component_type;
 
-HPX_REGISTER_MINIMAL_COMPONENT_FACTORY(hdb_component_type, hdb_component);
+HPX_REGISTER_MINIMAL_COMPONENT_FACTORY_DYNAMIC(hdb_component_type, hdb_component);
 
 ///////////////////////////////////////////////////////////////////////////////
 // Serialization support for simple_accumulator actions.
